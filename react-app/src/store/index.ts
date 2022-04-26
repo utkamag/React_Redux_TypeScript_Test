64d9ff0e98a3435230179f4bef078a5950510841
utkamag <92789705+utkamag@users.noreply.github.com>
2022-04-26 13:37:50 +0400

    update code

--- react-app/src/store/index.ts
+++ react-app/src/store/index.ts
@@ -1,6 +1,7 @@
 import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
+import userReducer from "./reducers/userReducer";
 
-const store = createStore({useReducer}, applyMiddleware(thunk))
+const store = createStore({userReducer}, applyMiddleware(thunk))
 
 export default store
--- react-app/src/store/reducers/userReducer.ts
+++ react-app/src/store/reducers/userReducer.ts
@@ -1,10 +1,26 @@
 
+// Вывносим action в отдельную константу
+
+const FETCH_USERS = "FETCH_USERS"
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS"
+const FETCH_USERS_ERROR = "FETCH_USERS_ERROR"
+
+// Интерфейс стейта
+
 interface UserState {
     users: any[];
     loading: boolean;
     error: null | string;
 }
 
+// Интерфейс action
+
+interface UserAction {
+    type: string;
+    payload?: any;
+}
+
+// state редьюсера
 
 const initialState: UserState = {
     users: [],
@@ -13,10 +29,16 @@ const initialState: UserState = {
 }
 
 
-const userReducer = (state = initialState, action) => {
+const userReducer = (state = initialState, action: UserAction): UserState => {
     switch (action.type){
-        case "FETCH_USERS":
-            return { users: action.payload }
+        case FETCH_USERS:
+            return { loading: true, error: null, users: [] }
+        case FETCH_USERS_SUCCESS:
+            return { loading: false, error: null, users: action.payload}
+        case FETCH_USERS_ERROR:
+            return { loading: false, error: action.payload, users: [] }
+        default:
+            return state
     }
 }
 
